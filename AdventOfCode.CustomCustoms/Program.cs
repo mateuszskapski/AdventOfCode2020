6115309a9406ae85f5907b3426aac04933b58ec2
Mateusz Skapski <mateusz.skapski@hagueprint.com>
2020-12-07 18:10:12 +0000

    Initial commit day 6

---
+++ AdventOfCode.CustomCustoms/Program.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.CustomCustoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser parser = new FileParser(Path.Combine(PathHelper.ProjectRootFolder(), "Input.txt"), $"{Environment.NewLine}{Environment.NewLine}");
+            var groups = parser.ToStringList();
+
+            int yesAnswersCount = CountAllYesAnswers(groups);
+            Console.WriteLine($"(Yes) answers: {yesAnswersCount}");
+            
+            int sameQuestionAnswersCount = CountSameQuestionAnswers(groups);
+            Console.WriteLine($"(Yes) same questions answers: {sameQuestionAnswersCount}");
+        }
+
+        private static int CountSameQuestionAnswers(List<string> groups)
+        {
+            int sameQuestionAnswersCount = 0;
+            foreach (var group in groups)
+            {
+                int groupMembersCount = group.Split(Environment.NewLine).Length;
+
+                var answersCountSameQuestion = group.Replace("\r\n", string.Empty)
+                                                    .GroupBy(x => x)
+                                                    .Select(x => new { Letter = x.Key, Count = x.Count() })
+                                                    .Where(x => x.Count == groupMembersCount)
+                                                    .OrderBy(x => x.Letter)
+                                                    .ToList();
+
+                sameQuestionAnswersCount += answersCountSameQuestion.Select(x => x).Count();
+            }
+
+            return sameQuestionAnswersCount;
+        }
+
+        private static int CountAllYesAnswers(List<string> groups)
+        {
+            int answersCount = 0;
+            foreach (var group in groups)
+            {
+                answersCount += group.Replace("\r\n", string.Empty).Select(x => x).Distinct().Count();
+            }
+
+            return answersCount;
+        }
+    }
+}
+
+// psyjxulrdtfejeusdrlxyftpufdpjsxrlztyeyeorabxsdnhftujlppedfxtsryujl
