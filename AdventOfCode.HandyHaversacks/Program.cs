e86e052e3d10ea9b990d4fd1d39bbe754b80bc86
Mateusz Skapski <mateusz.skapski@hagueprint.com>
2020-12-08 17:43:34 +0000

    Initial commit day 7

---
+++ AdventOfCode.HandyHaversacks/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.IO;
+
+namespace AdventOfCode.HandyHaversacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser fileParser1 = new FileParser(Path.Combine(PathHelper.ProjectRootFolder(), "Input.txt"), Environment.NewLine);
+        }
+    }
+}
