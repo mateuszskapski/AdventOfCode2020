25f3550c543c74a895efad52a11526d52ce7388c
Mateusz Skapski <mateusz.skapski@hagueprint.com>
2020-12-19 11:43:11 +0000

    Day 9 initial commit

---
+++ EncodingError/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EncodingError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll<string, ulong>(File.ReadAllText(Path.Combine(PathHelper.ProjectRootFolder(), "Input.txt")).Split(Environment.NewLine), x => UInt64.Parse(x)).ToList();
+        }
+    }
+}
