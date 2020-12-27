9f4baa384bb3d357d50cfe69acbe0fdb9ec42a65
Mateusz Skapski <mats.sk@gmail.com>
2020-12-27 22:41:25 +0000

    Part two solved

--- AdapterArray/Program.cs
+++ AdapterArray/Program.cs
@@ -21,6 +21,29 @@ namespace AdapterArray
 
             Console.WriteLine($"Diff of 1: {ratings[1]}, diff of 3: {ratings[3] + 1}");
             Console.WriteLine($"Part one answer is: {ratings[1] * (ratings[3] +1)}");
+
+            input.Insert(0, 0);
+            input.Insert(input.Count, input.Last() + 3);
+            int combinationsCount = CountCombinations(input);
+        }
+
+        private static int CountCombinations(List<int> input)
+        {
+            int count = 0;
+            SequenceFinder sequenceFinder = new SequenceFinder();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                sequenceFinder.Add(input[i]);
+            }
+
+            foreach (var range in sequenceFinder.GetSequences())
+            {
+                Console.WriteLine($"S: {range.NumStart} E: {range.NumEnd} Count: {range.Count} Combinations: {range.CombinationCount}");
+            }
+            Console.WriteLine($"Total adapter combinations: {sequenceFinder.GetSequences().Select(x => x.CombinationCount).Aggregate(1, (long x, int y) => x * y)}");
+
+            return count;
         }
 
         private static void CountDifferences(int rating, ref int previousRating, ref Dictionary<int, int> ratings)
@@ -35,6 +58,63 @@ namespace AdapterArray
             }
 
             previousRating = rating;
+        }
+
+        class Range
+        {
+            public int NumStart { get; set; }
+            public int NumEnd { get; set; }
+            public int Count { get; set; }
+            public int CombinationCount { get; set; } = 1;
+
+            public Range(int numStart, int numEnd)
+            {
+                NumStart = numStart;
+                NumEnd = numEnd;
+                Count = numEnd - numStart - 1;
+                
+                for (int i = Count; i > 0; i--)
+                {
+                    CombinationCount += i;
+                }
+            }
+        }
+
+        class SequenceFinder
+        {
+            private int _sequenceStart = 0;
+            private int _previousNum = 0;
+
+            private List<Range> _ranges = new List<Range>();
+
+            public void Add(int num)
+            {
+                if (num == 0)
+                {
+                    _sequenceStart = 0;
+                    _previousNum = 0;
+
+                    return; 
+                }
+
+                if (num - _previousNum == 1)
+                {
+                    _previousNum = num;
+                    return;
+                }
+                else
+                {
+                    if (_previousNum > _sequenceStart)
+                    {
+                        _ranges.Add(new Range(_sequenceStart, _previousNum));
+                    }
+
+                    _previousNum = num;
+                    _sequenceStart = num;
+                }
+            }
+
+            public List<Range> GetSequences() => _ranges;
         }
     }
 }
