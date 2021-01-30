ca7696b35a6a7f0d210503004fd59f380cb45447
Mateusz Skapski <mateusz.skapski@hagueprint.com>
2021-01-30 23:19:19 +0000

    Commit before reinstallation of the system

--- AdventOfCode.ShuttleSearch/Program.cs
+++ AdventOfCode.ShuttleSearch/Program.cs
@@ -11,59 +11,65 @@ namespace AdventOfCode.ShuttleSearch
         {
             var input = File.ReadAllText(Path.Combine(PathHelper.ProjectRootFolder(), "Input.txt")).Split(Environment.NewLine);
 
-            //int currentTimestamp = int.Parse(input[0]);
-            //var nextBus = input[1].Replace("x,", string.Empty)
-            //    .Split(',')
-            //    .Select(int.Parse)
-            //    .Select(b => (b, currentTimestamp - (currentTimestamp % b) + b))
-            //    .OrderBy(b => b.Item2)
-            //    .Where(x => x.Item2 > currentTimestamp)
-            //    .First();
+            //PartOne(input);
 
-            //Console.WriteLine($"Next bus ID: {nextBus.Item1} is at: {nextBus.Item2}");
-            //Console.WriteLine($"Part one answer: {(nextBus.Item2 - currentTimestamp) * nextBus.Item1}");
+            PartTwo(input);
+            
 
-            var buses = input[1].Replace("x,", string.Empty)
+            static void PartOne(string[] input)
+            {
+                int currentTimestamp = int.Parse(input[0]);
+                var nextBus = input[1].Replace("x,", string.Empty)
+                    .Split(',')
+                    .Select(int.Parse)
+                    .Select(b => (b, currentTimestamp - (currentTimestamp % b) + b))
+                    .OrderBy(b => b.Item2)
+                    .Where(x => x.Item2 > currentTimestamp)
+                    .First();
+
+                Console.WriteLine($"Next bus ID: {nextBus.Item1} is at: {nextBus.Item2}");
+                Console.WriteLine($"Part one answer: {(nextBus.Item2 - currentTimestamp) * nextBus.Item1}");
+            }
+
+            static void PartTwo(string[] input)
+            {
+                var buses = input[1].Replace("x,", "0")
                 .Split(',')
-                .Select(ulong.Parse)
+                .Select(long.Parse)
                 .ToList();
 
-            foreach (var bus in buses)
-            {
-                Console.Write($"{bus}  ");
-            }
+                /*
+                    23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,829,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,677,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19
+                */
 
-            for (int i = 0; i < buses.Count(); i++)
-            {
-                Console.WriteLine(buses[i]);
-                while (i < buses.Count() - 1 ? buses[i] + buses[i] < (buses[i + 1]) : buses[i] > buses[i - 1] + buses[i - 1] ||
-                    i > 0 ? buses[i] > buses[i-1] : buses[i] < buses[i+1])
+                bool keepGoing = true;
+                while (keepGoing)
                 {
-                    buses[i] += buses[i];
-                }
+                    var maxTimestampBus = buses.Where(b => b.Item2 == buses.Select(x => x.Item2).Max()).Select(b => b).Max();
 
-                int counter = 0;
-                for (int j = 0; j < buses.Count(); j++)
-                {
-                    if (j < buses.Count() - 1 ? buses[j] < buses[j + 1] : buses[j] > buses[j - 1])
+                    var lastBus = buses.Select(b => b).Last();
+
+                    if (lastBus.Item2 < maxTimestampBus.Item2 && lastBus.Item1 != maxTimestampBus.Item1)
                     {
-                        counter++;
+                        lastBus = (lastBus.Item1,lastBus.Item1 * (maxTimestampBus.Item2 / lastBus.Item1) + lastBus.Item1);
+                    }
+
+                    ulong nextTimestamp = 0;
+                    for (int i = buses.Count - 1; i >= 0; i--)
+                    {
+                        if (nextTimestamp == 0)
+                        {
+                            nextTimestamp = buses[i].Item2;
+                            continue;
+                        }
+
+                        if (buses[i].Item2 > nextTimestamp)
+                        {
+
+                        }
                     }
                 }
-
-                if (counter == buses.Count)
-                {
-                    Console.WriteLine($"Nearest timestamp: {buses[i]}");
-                    break;
-                }
-
-                if (i+1 == buses.Count)
-                {
-                    i = 0;
-                }
             }
-
-
 
         }
     }
